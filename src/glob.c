eaf83d4cae00ae2e2ccccc3c02d5262330a31c8a
John Darrington <john@darrington.wattle.id.au>
2003-12-12 08:32:08 +0000

    Put back a bug fix which accidently got reverted.

--- src/glob.c
+++ src/glob.c
@@ -133,9 +133,7 @@ init_glob (int argc unused, char **argv)
 {
   /* FIXME: Allow i18n of other locale items (besides LC_MESSAGES). */
 #if ENABLE_NLS
-#if LC_MESSAGE
   setlocale (LC_MESSAGES, "");
-#endif
   setlocale (LC_MONETARY, "");
   bindtextdomain (PACKAGE, locale_dir);
   textdomain (PACKAGE);
