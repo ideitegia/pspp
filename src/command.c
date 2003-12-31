6bc566408707e018674d1776d835c78368b6b5a3
Ben Pfaff <blp@gnu.org>
2003-12-31 06:46:22 +0000

    Tue Dec 30 22:42:57 2003  Ben Pfaff  <blp@gnu.org>
    	* Removed bletcherous alloca() workarounds for AIX from top of
    	many files.  AIX can use the alternative alloca() implementation
    	instead.

--- src/command.c
+++ src/command.c
@@ -17,24 +17,7 @@
    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
    02111-1307, USA. */
 
-/* AIX requires this to be the first thing in the file.  */
 #include <config.h>
-#if __GNUC__
-#define alloca __builtin_alloca
-#else
-#if HAVE_ALLOCA_H
-#include <alloca.h>
-#else
-#ifdef _AIX
-#pragma alloca
-#else
-#ifndef alloca			/* predefined by HP cc +Olibcalls */
-char *alloca ();
-#endif
-#endif
-#endif
-#endif
-
 #include <assert.h>
 #include "command.h"
 #include <stdio.h>
--- src/crosstabs.q
+++ src/crosstabs.q
@@ -29,24 +29,7 @@
 
 */
 
-/* AIX requires this to be the first thing in the file.  */
 #include <config.h>
-#if __GNUC__
-#define alloca __builtin_alloca
-#else
-#if HAVE_ALLOCA_H
-#include <alloca.h>
-#else
-#ifdef _AIX
-#pragma alloca
-#else
-#ifndef alloca			/* predefined by HP cc +Olibcalls */
-char *alloca ();
-#endif
-#endif
-#endif
-#endif
-
 #include <assert.h>
 #include <ctype.h>
 #include <stdlib.h>
