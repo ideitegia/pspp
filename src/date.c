8798f80b8e8bf37d881dd10f2da88c4066d27bea
Ben Pfaff <blp@gnu.org>
2004-03-21 04:23:49 +0000

    Oops, add initial version.

---
+++ src/date.c
@@ -0,0 +1,34 @@
+/* PSPP - computes sample statistics.
+   Copyright (C) 2004 Free Software Foundation, Inc.
+   Written by Ben Pfaff <blp@gnu.org>.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA. */
+
+#include <config.h>
+#include "command.h"
+#include "error.h"
+#include "lexer.h"
+
+/* Stub for USE command. */
+int
+cmd_use (void) 
+{
+  if (lex_match (T_ALL))
+    return lex_end_of_command ();
+
+  msg (SW, _("Only USE ALL is currently implemented."));
+  return CMD_FAILURE;
+}
