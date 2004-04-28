c912ba11ed1d84c40085177439ac7086f2a43b20
pjk <pjk>
2004-04-28 16:03:45 +0000

    - Add test for compute heap munching bug (compute a=upcase(a)) where a is a   large string

---
+++ tests/bugs/computebug.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# This program tests ....
+
+TEMPDIR=/tmp/pspp-tst-$$
+
+here=`pwd`;
+
+# ensure that top_srcdir is absolute
+cd $top_srcdir; top_srcdir=`pwd`
+
+export STAT_CONFIG_PATH=$top_srcdir/config
+
+
+cleanup()
+{
+     rm -rf $TEMPDIR
+}
+
+
+fail()
+{
+    echo $activity
+    echo FAILED
+    cleanup;
+    exit 1;
+}
+
+
+no_result()
+{
+    echo $activity
+    echo NO RESULT;
+    cleanup;
+    exit 2;
+}
+
+pass()
+{
+    cleanup;
+    exit 0;
+}
+
+mkdir -p $TEMPDIR
+cp $top_srcdir/tests/bugs/computebug.stat $TEMPDIR
+cd $TEMPDIR
+
+
+if [ $? -ne 0 ] ; then no_result ; fi
+
+
+activity="run program"
+$SUPERVISOR $top_srcdir/src/pspp -o raw-ascii $TEMPDIR/computebug.stat
+if [ $? -ne 0 ] ; then no_result ; fi
+
+
+activity="compare output"
+diff -b -B -w $TEMPDIR/pspp.list $top_srcdir/tests/bugs/computebug.out
+
+if [ $? -ne 0 ] ; then fail ; fi
+
+pass;
