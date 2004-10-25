2ff8d56f90bf689b69f3314ba12d03f6bf62c95f
John Darrington <john@darrington.wattle.id.au>
2004-10-25 03:47:35 +0000

    Added this file which I forgot.

---
+++ src/oneway.h
@@ -0,0 +1,43 @@
+/* PSPP - computes sample statistics.
+
+   Copyright (C) 2004 Free Software Foundation, Inc.
+
+   Written by John Darrington <john@darrington.wattle.id.au>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA. */
+
+#ifndef ONEWAY_H
+#define ONEWAY_H
+
+/* ONEWAY private data */
+struct oneway_proc
+{
+  /* Stats for the `universal group'  (ie the totals) */
+  struct group_statistics ugs;
+
+  /* The number of groups */
+  int n_groups;
+
+  /* A hash of group statistics keyed by the value of the 
+     independent variable */
+  struct hsh_table *group_hash;
+
+  /* Mean square error */
+  double mse ; 
+
+};
+
+#endif
