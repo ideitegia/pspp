6e5a7c31f66ca6209c3be2d495c14e880964c3d4
Ben Pfaff <blp@gnu.org>
2005-07-31 05:51:35 +0000

    Include <config.h>.

--- src/histogram.c
+++ src/histogram.c
@@ -17,6 +17,7 @@
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
    02110-1301, USA. */
 
+#include <config.h>
 #include <math.h>
 #include <gsl/gsl_histogram.h>
 #include <assert.h>
