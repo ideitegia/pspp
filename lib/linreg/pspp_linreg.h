9703148a4b6ae39bd7ff79741020dac46018a7f7
Jason Stover <jhs@math.gcsu.edu>
2005-09-17 15:55:56 +0000

    Initial version

---
+++ lib/linreg/pspp_linreg.h
@@ -0,0 +1,161 @@
+/* lib/linreg/linreg.c
+
+   Copyright (C) 2005 Free Software Foundation, Inc.
+   Written by Jason H Stover.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02111-1307, USA.
+ */
+
+/*
+  Find the least-squares estimate of b for the linear model:
+
+  Y = Xb + Z
+
+  where Y is an n-by-1 column vector, X is an n-by-p matrix of 
+  independent variables, b is a p-by-1 vector of regression coefficients,
+  and Z is an n-by-1 normally-distributed random vector with independent
+  identically distributed components with mean 0.
+
+  This estimate is found via the sweep operator or singular-value
+  decomposition.
+
+
+  References:
+
+  Matrix Computations, third edition. GH Golub and CF Van Loan.
+  The Johns Hopkins University Press. 1996. ISBN 0-8018-5414-8.
+
+  Numerical Analysis for Statisticians. K Lange. Springer. 1999.
+  ISBN 0-387-94979-8.
+
+  Numerical Linear Algebra for Applications in Statistics. JE Gentle.
+  Springer. 1998. ISBN 0-387-98542-5.
+ */
+#ifndef PSPP_LINREG_H
+#define PSPP_LINREG_H
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_fit.h>
+#include <gsl/gsl_multifit.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_cblas.h>
+enum
+{
+  PSPP_LINREG_SWEEP,
+  PSPP_LINREG_SVD
+};
+
+/*
+  Cache for the relevant data from the model. There are several
+  members which the caller may not use, and which could use a lot of
+  storage. Therefore non-essential members of the struct will be
+  allocated only when requested.
+ */
+struct pspp_linreg_cache_struct
+{
+  int n_obs;			/* Number of observations. */
+  int n_indeps;			/* Number of independent variables. */
+  gsl_vector *depvar;
+  gsl_matrix *indepvar;
+  gsl_vector *residuals;
+  gsl_vector *param_estimates;
+  int method;			/* Method to use to estimate parameters. */
+  /*
+     Means and standard deviations of the variables.
+     If these pointers are null when pspp_linreg() is
+     called, pspp_linreg() will compute their values.
+
+     Entry i of indep_means is the mean of independent
+     variable i, whose observations are stored in column i
+     of indepvar.
+   */
+  double depvar_mean;
+  double depvar_std;
+  gsl_vector *indep_means;
+  gsl_vector *indep_std;
+
+  /*
+     Sums of squares.
+   */
+  double ssm;			/* Sums of squares for the overall model. */
+  gsl_vector *ss_indeps;	/* Sums of squares from each 
+				   independent variable. 
+				 */
+  double sst;			/* Sum of squares total. */
+  double sse;			/* Sum of squares error. */
+  double mse;			/* Mean squared error. This is just sse / dfe, but
+				   since it is the best unbiased estimate of the population
+				   variance, it has its own entry here.
+				 */
+  gsl_vector *ssx;		/* Centered sums of squares for independent variables,
+				   i.e. \sum (x[i] - mean(x))^2. 
+				 */
+  double ssy;			/* Centered sums of squares for dependent variable. */
+  /*
+     Covariance matrix of the parameter estimates.
+   */
+  gsl_matrix *cov;
+  /*
+     Degrees of freedom.
+   */
+  double dft;
+  double dfe;
+  double dfm;
+
+  /*
+     'Hat' or Hessian matrix, i.e. (X'X)^{-1}, where X is our
+     design matrix.
+   */
+  gsl_matrix *hat;
+};
+typedef struct pspp_linreg_cache_struct pspp_linreg_cache;
+
+/*
+  Options describing what special values should be computed.
+ */
+struct pspp_linreg_opts_struct
+{
+  int resid;			/* Should the residuals be returned? */
+
+  int get_depvar_mean_std;
+  int *get_indep_mean_std;	/* Array of booleans dictating which
+				   independent variables need their means
+				   and standard deviations computed within
+				   pspp_linreg. This array MUST be of
+				   length n_indeps. If element i is 1,
+				   pspp_linreg will compute the mean and
+				   variance of indpendent variable i. If
+				   element i is 0, it will not compute the
+				   mean and standard deviation, and assume
+				   the values are stored.
+				   cache->indep_mean[i] is the mean and
+				   cache->indep_std[i] is the sample
+				   standard deviation.
+				 */
+};
+typedef struct pspp_linreg_opts_struct pspp_linreg_opts;
+
+int pspp_reg_sweep (gsl_matrix * A);
+
+pspp_linreg_cache *pspp_linreg_cache_alloc (size_t n, size_t p);
+
+void pspp_linreg_cache_free (pspp_linreg_cache * cache);
+
+int pspp_linreg (const gsl_vector * Y, const gsl_matrix * X,
+		 const pspp_linreg_opts * opts, pspp_linreg_cache * cache);
+#endif
---
+++ lib/linreg/sweep.c
@@ -0,0 +1,155 @@
+/* lib/linreg/sweep.c
+
+ Copyright (C) 2005 Free Software Foundation, Inc.
+ Written by Jason H Stover.
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or (at
+ your option) any later version.
+
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ 02111-1307, USA.
+ */
+
+/*
+  Find the least-squares estimate of b for the linear model:
+
+  Y = Xb + Z
+
+  where Y is an n-by-1 column vector, X is an n-by-p matrix of 
+  independent variables, b is a p-by-1 vector of regression coefficients,
+  and Z is an n-by-1 normally-distributed random vector with independent
+  identically distributed components with mean 0.
+
+  This estimate is found via the sweep operator, which is a modification
+  of Gauss-Jordan pivoting.
+
+
+  References:
+
+  Matrix Computations, third edition. GH Golub and CF Van Loan.
+  The Johns Hopkins University Press. 1996. ISBN 0-8018-5414-8.
+
+  Numerical Analysis for Statisticians. K Lange. Springer. 1999.
+  ISBN 0-387-94979-8.
+
+  Numerical Linear Algebra for Applications in Statistics. JE Gentle.
+  Springer. 1998. ISBN 0-387-98542-5.
+ */
+
+#include "pspp_linreg.h"
+
+/*
+  The matrix A will be overwritten. In ordinary uses of the sweep
+  operator, A will be the matrix
+
+   __       __
+  |X'X    X'Y|
+  |          |
+  |Y'X    Y'Y|
+   --        --
+
+   X refers to the design matrix and Y to the vector of dependent
+   observations. pspp_reg_sweep sweeps on the diagonal elements of 
+   X'X.
+
+   The matrix A is assumed to be symmetric, so the sweep operation is
+   performed only for the upper triangle of A.
+ */
+
+int
+pspp_reg_sweep (gsl_matrix * A)
+{
+  double sweep_element;
+  double tmp;
+  int i;
+  int j;
+  int k;
+  gsl_matrix *B;
+
+  if (A != NULL)
+    {
+      if (A->size1 == A->size2)
+	{
+	  B = gsl_matrix_alloc (A->size1, A->size2);
+	  for (k = 0; k < (A->size1 - 1); k++)
+	    {
+	      sweep_element = gsl_matrix_get (A, k, k);
+	      if (fabs (sweep_element) > GSL_DBL_MIN)
+		{
+		  tmp = -1.0 / sweep_element;
+		  gsl_matrix_set (B, k, k, tmp);
+		  /*
+		     Rows before current row k.
+		   */
+		  for (i = 0; i < k; i++)
+		    {
+		      for (j = i; j < A->size2; j++)
+			{
+			  /*
+			     Use only the upper triangle of A. 
+			   */
+			  if (j < k)
+			    {
+			      tmp = gsl_matrix_get (A, i, j) -
+				gsl_matrix_get (A, i, k)
+				* gsl_matrix_get (A, j, k) / sweep_element;
+			      gsl_matrix_set (B, i, j, tmp);
+			    }
+			  else if (j > k)
+			    {
+			      tmp = gsl_matrix_get (A, i, j) -
+				gsl_matrix_get (A, i, k)
+				* gsl_matrix_get (A, k, j) / sweep_element;
+			      gsl_matrix_set (B, i, j, tmp);
+			    }
+			  else
+			    {
+			      tmp = gsl_matrix_get (A, i, k) / sweep_element;
+			      gsl_matrix_set (B, i, j, tmp);
+			    }
+			}
+		    }
+		  /*
+		     Current row k.
+		   */
+		  for (j = k + 1; j < A->size1; j++)
+		    {
+		      tmp = gsl_matrix_get (A, k, j) / sweep_element;
+		      gsl_matrix_set (B, k, j, tmp);
+		    }
+		  /*
+		     Rows after the current row k.
+		   */
+		  for (i = k + 1; i < A->size1; i++)
+		    {
+		      for (j = i; j < A->size2; j++)
+			{
+			  tmp = gsl_matrix_get (A, i, j) -
+			    gsl_matrix_get (A, k, i)
+			    * gsl_matrix_get (A, k, j) / sweep_element;
+			  gsl_matrix_set (B, i, j, tmp);
+			}
+		    }
+		}
+	      for (i = 0; i < A->size1; i++)
+		for (j = i; j < A->size2; j++)
+		  {
+		    gsl_matrix_set (A, i, j, gsl_matrix_get (B, i, j));
+		  }
+	    }
+	  gsl_matrix_free (B);
+	  return GSL_SUCCESS;
+	}
+      return GSL_ENOTSQR;
+    }
+  return GSL_EFAULT;
+}
