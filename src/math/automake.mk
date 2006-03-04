bcf41af107e8dffb506a506f576c6535d1b3bde7
John Darrington <john@darrington.wattle.id.au>
2006-03-04 01:23:16 +0000

    Added files in src/math

---
+++ src/math/automake.mk
@@ -0,0 +1,27 @@
+## Process this file with automake to produce Makefile.in  -*- makefile -*-
+
+include $(top_srcdir)/src/math/linreg/automake.mk
+
+src/math/%: AM_CPPFLAGS += \
+	-I$(top_srcdir)/src/math \
+	-I$(top_srcdir)/src/libpspp \
+	-I$(top_srcdir)/src/data 
+
+
+noinst_LIBRARIES += src/math/libpspp_math.a
+
+src_math_libpspp_math_a_SOURCES = \
+	src/math/factor-stats.c \
+	src/math/factor-stats.h \
+	src/math/chart-geometry.c \
+	src/math/chart-geometry.h \
+	src/math/group.c  src/math/group.h \
+	src/math/histogram.c src/math/histogram.h \
+	src/math/group-proc.h \
+	src/math/levene.c \
+	src/math/levene.h \
+	src/math/moments.c  src/math/moments.h \
+	src/math/percentiles.c src/math/percentiles.h \
+	src/math/design-matrix.c src/math/design-matrix.h \
+	src/math/random.c src/math/random.h \
+	src/math/sort.c src/math/sort.h 
