fb4554347cc7b5d85a04213ea3747ef3a34ba2a4
Ben Pfaff <blp@gnu.org>
2006-03-31 18:38:59 +0000

    Add freaderror() analogous to fwriteerror() in gnulib.

---
+++ src/libpspp/freaderror.h
@@ -0,0 +1,22 @@
+/* Detect read error on a stream.
+   Copyright (C) 2003, 2005, 2006 Free Software Foundation, Inc.
+   Written by Bruno Haible <bruno@clisp.org>, 2003.
+   Modified by Ben Pfaff <blp@cs.stanford.edu> for PSPP.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+#include <stdio.h>
+
+extern int freaderror (FILE *fp);
