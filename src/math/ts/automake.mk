74fc00cc0eb769ab61141d290490037ba040f63a
Jason Stover <jhs@math.gcsu.edu>
2006-06-07 01:17:08 +0000

    initial version

---
+++ src/math/ts/automake.mk
@@ -0,0 +1,7 @@
+## Process this file with automake to produce Makefile.in  -*- makefile -*-
+
+noinst_LIBRARIES += src/math/ts/libpspp_ts.a
+
+src_math_ts_libpspp_ts_a_SOURCES = \
+	src/math/ts/innovations.c \
+	src/math/ts/innovations.h
