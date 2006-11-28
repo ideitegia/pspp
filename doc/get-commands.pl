85cef9b9391f1aaf4b27bd90a02f8ce5daf004ee
John Darrington <john@darrington.wattle.id.au>
2006-11-28 09:39:37 +0000

    Fixed documentation problems

--- doc/get-commands.pl
+++ doc/get-commands.pl
@@ -1,28 +1,32 @@
-
-#
+#!/usr/bin/perl
 # Creates Texinfo documentation from the source 
 
 $file = $ARGV[0]; 
 open(INFO, $file) || die "Cannot open \"$file\"\n" ;	
+print "\@c Generated from $file by get-commands.pl\n";
+print "\@c Do not modify!\n\n";
+
 print "\@table \@asis\n";
 while ($line = <INFO>)
 {
-	if ( $line =~ /^UNIMPL/ ) 
-	{
-		@fields = split(/,/,$line); 
-		$_ = $fields[0];
-		s/^UNIMPL//;
-		s/ *\(\"// ;
-		s/\"//;
-		$command = $_;
-		$_=$fields[5];
-		s/\"//;
-		s/\"\)//;
-		s/^ *//;
-		chomp;
-		$description = $_;
-		print "\@item $command\n$description\n\n";
-	}
+    if ( $line =~ /^UNIMPL_CMD/ ) 
+    {
+	@fields = split(/,/,$line); 
+	$_ = $fields[0];
+	s/^UNIMPL_CMD//;
+	s/ *\(\"// ;
+	s/\"//;
+	$command = $_;
+	$_=$fields[1];
+	s/^ *\"//;
+	s/\"\)//;
+	chomp;
+	$description = $_;
+	print "\@item $command\n$description\n\n";
+    }
 }
 print "\@end table\n";
+
+print "\@c Local Variables:\n";
+print "\@c buffer-read-only: t\n";
 close(INFO);			# Close the file
