de5fae9e869d6d6e32a5bf130ba4d28799eb75df
John Darrington <john@darrington.wattle.id.au>
2006-12-22 21:46:12 +0000

    Checked in missed file.  Thanks to Ben Pfaff for the reminder.

---
+++ tests/bugs/freq-nolabels.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+# This program tests for a bug which crashed frequencies when the
+# /FORMAT=nolabels was given.
+
+TEMPDIR=/tmp/pspp-tst-$$
+TESTFILE=$TEMPDIR/`basename $0`.sps
+
+# ensure that top_srcdir and top_builddir  are absolute
+if [ -z "$top_srcdir" ] ; then top_srcdir=. ; fi
+if [ -z "$top_builddir" ] ; then top_builddir=. ; fi
+top_srcdir=`cd $top_srcdir; pwd`
+top_builddir=`cd $top_builddir; pwd`
+
+PSPP=$top_builddir/src/ui/terminal/pspp
+
+STAT_CONFIG_PATH=$top_srcdir/config
+export STAT_CONFIG_PATH
+
+LANG=C
+export LANG
+
+
+cleanup()
+{
+     if [ x"$PSPP_TEST_NO_CLEANUP" != x ] ; then 
+	echo "NOT cleaning $TEMPDIR"
+     	return ; 
+     fi
+     rm -rf $TEMPDIR
+}
+
+
+fail()
+{
+    echo $activity
+    echo FAILED
+    cleanup;
+    exit 1;
+}
+
+
+no_result()
+{
+    echo $activity
+    echo NO RESULT;
+    cleanup;
+    exit 2;
+}
+
+pass()
+{
+    cleanup;
+    exit 0;
+}
+
+mkdir -p $TEMPDIR
+
+cd $TEMPDIR
+
+activity="create script"
+cat > $TESTFILE  <<EOF
+DATA LIST LIST /x * w *.
+BEGIN DATA.
+1 4
+2 2
+END DATA.
+
+WEIGHT BY w.
+
+FREQUENCIES x
+  /FORMAT=nolabels
+  .
+EOF
+if [ $? -ne 0 ] ; then no_result ; fi
+
+
+activity="run program"
+$SUPERVISOR $PSPP --testing-mode -o raw-ascii $TESTFILE
+if [ $? -ne 0 ] ; then no_result ; fi
+
+
+
+pass;
