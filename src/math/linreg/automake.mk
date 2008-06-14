51010fcf5cc308367c1fe3fafa785a3d2216b03d
Jason Stover <jhs@math.gcsu.edu>
2008-06-14 15:38:24 -0400

    removed src/math/linreg/

--- src/math/linreg/automake.mk
+++
@@ -1,9 +0,0 @@
-## Process this file with automake to produce Makefile.in  -*- makefile -*-
-
-noinst_LIBRARIES += src/math/linreg/libpspp_linreg.a
-
-src_math_linreg_libpspp_linreg_a_SOURCES = \
-	src/math/linreg/predict.c \
-	src/math/linreg/linreg.c \
-	src/math/linreg/linreg.h 
-
