cdcdc851b79c8f9ddcaaa358a80670bbb21d61f2
John Darrington <john@darrington.wattle.id.au>
2009-03-02 20:56:30 +0900

    Set icon for text import assistant window

--- src/ui/gui/text-data-import-dialog.c
+++ src/ui/gui/text-data-import-dialog.c
@@ -589,6 +589,7 @@ init_assistant (struct import_assistant *ia, GtkWindow *parent_window)
   gtk_window_set_title (GTK_WINDOW (a->assistant),
                         _("Importing Delimited Text Data"));
   gtk_window_set_transient_for (GTK_WINDOW (a->assistant), parent_window);
+  gtk_window_set_icon_name (GTK_WINDOW (a->assistant), "psppicon");
 
   a->prop_renderer = gtk_cell_renderer_text_new ();
   g_object_ref_sink (a->prop_renderer);
