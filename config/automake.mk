dbb0b9deb0d6351d65ced04f1e3f6d35cd0251e7
Ben Pfaff <blp@gnu.org>
2009-12-07 21:01:41 -0800

    Remove AFM files.
    Now that the PostScript driver is gone, there is no longer any need for
    these.
    
    Thanks to John Darrington <john@cellform.com> for pointing out that they
    are no longer necessary.

--- config/automake.mk
+++ config/automake.mk
@@ -4,19 +4,4 @@
 dist_pkgsysconf_DATA = \
 	config/devices
 
-psfontsdir = $(pkgsysconfdir)/psfonts
-dist_psfonts_DATA = \
-	config/psfonts/Helvetica-Bold.afm \
-	config/psfonts/Times-Bold.afm \
-	config/psfonts/Courier-Bold.afm \
-	config/psfonts/Helvetica-BoldOblique.afm \
-	config/psfonts/Times-BoldItalic.afm \
-	config/psfonts/Courier-BoldOblique.afm \
-	config/psfonts/Helvetica-Oblique.afm \
-	config/psfonts/Times-Italic.afm \
-	config/psfonts/Courier-Oblique.afm \
-	config/psfonts/Helvetica.afm \
-	config/psfonts/Times-Roman.afm \
-	config/psfonts/Courier.afm
-
 EXTRA_DIST += config/OChangeLog
