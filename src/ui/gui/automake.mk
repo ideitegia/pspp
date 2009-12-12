bb025f49673d66a2e490a781d7d59b2f32da0087
John Darrington <john@darrington.wattle.id.au>
2009-12-12 16:48:02 +0100

    Implemented a dialog box for the ROC command

--- src/ui/gui/automake.mk
+++ src/ui/gui/automake.mk
@@ -67,6 +67,7 @@ UI_FILES = \
 	src/ui/gui/recode.ui \
 	src/ui/gui/regression.ui \
 	src/ui/gui/reliability.ui \
+	src/ui/gui/roc.ui \
 	src/ui/gui/t-test.ui \
 	src/ui/gui/text-data-import.ui \
 	src/ui/gui/var-sheet-dialogs.ui \
@@ -187,6 +188,8 @@ src_ui_gui_psppire_SOURCES = \
 	src/ui/gui/regression-dialog.h \
 	src/ui/gui/reliability-dialog.c \
 	src/ui/gui/reliability-dialog.h \
+	src/ui/gui/roc-dialog.c \
+	src/ui/gui/roc-dialog.h \
 	src/ui/gui/select-cases-dialog.c \
 	src/ui/gui/select-cases-dialog.h \
 	src/ui/gui/sort-cases-dialog.c \
--- src/ui/gui/psppire-data-window.c
+++ src/ui/gui/psppire-data-window.c
@@ -56,6 +56,7 @@
 #include "examine-dialog.h"
 #include "regression-dialog.h"
 #include "reliability-dialog.h"
+#include "roc-dialog.h"
 #include "oneway-anova-dialog.h"
 #include "t-test-independent-samples-dialog.h"
 #include "t-test-one-sample.h"
@@ -1690,6 +1691,20 @@ psppire_data_window_init (PsppireDataWindow *de)
 
     g_signal_connect (invoke_reliability_dialog, "activate",
 		      G_CALLBACK (reliability_dialog), de);
+  }
+
+  {
+    GtkAction *invoke_roc_dialog =
+      resolve_action (de->builder, "roc-curve", NULL);
+
+    g_object_set (invoke_roc_dialog,
+		  "tooltip", _("ROC Curve"),
+		  "stock-id", "pspp-roc",
+		  NULL
+		  );
+
+    g_signal_connect (invoke_roc_dialog, "activate",
+		      G_CALLBACK (roc_dialog), de);
   }
 
 
