4eac86bdd50377578553ca2aa2eb87199dbb0aa1
Ben Pfaff <blp@cs.stanford.edu>
2010-06-15 22:19:17 -0700

    psppire: Fix insecure temporary file creation in clipboard_get_cb().

--- src/ui/gui/psppire-output-window.c
+++ src/ui/gui/psppire-output-window.c
@@ -16,32 +16,30 @@
 
 #include <config.h>
 
-#include <gtk/gtksignal.h>
-#include <gtk/gtkbox.h>
-#include "helper.h"
-
-#include <libpspp/cast.h>
-#include <libpspp/message.h>
-#include <libpspp/string-map.h>
-#include <output/cairo.h>
-#include <output/chart-item.h>
-#include <output/driver-provider.h>
-#include <output/output-item.h>
-#include <output/table-item.h>
-#include <output/text-item.h>
-#include <output/tab.h>
+#include <errno.h>
+#include <gtk/gtk.h>
 #include <stdlib.h>
-
-#include "help-menu.h"
-
-#include "psppire-output-window.h"
-
-
-#include "xalloc.h"
-
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
+
+#include "libpspp/cast.h"
+#include "libpspp/message.h"
+#include "libpspp/string-map.h"
+#include "output/cairo.h"
+#include "output/chart-item.h"
+#include "output/driver-provider.h"
+#include "output/output-item.h"
+#include "output/tab.h"
+#include "output/table-item.h"
+#include "output/text-item.h"
+#include "ui/gui/help-menu.h"
+#include "ui/gui/helper.h"
+#include "ui/gui/psppire-output-window.h"
+
+#include "gl/error.h"
+#include "gl/tmpdir.h"
+#include "gl/xalloc.h"
 
 #include <gettext.h>
 #define _(msgid) gettext (msgid)
@@ -801,7 +799,7 @@ clipboard_get_cb (GtkClipboard     *clipboard,
   gsize length;
   gchar *text = NULL;
   struct output_driver *driver = NULL;
-  char *filename = NULL;
+  char dirname[PATH_MAX], *filename;
   struct string_map options;
 
   GtkTreeSelection *sel = gtk_tree_view_get_selection (window->overview);
@@ -813,8 +811,15 @@ clipboard_get_cb (GtkClipboard     *clipboard,
   if ( n == NULL)
     return;
 
+  if (path_search (dirname, sizeof dirname, NULL, NULL, true)
+      || mkdtemp (dirname) == NULL)
+    {
+      error (0, errno, _("failed to create temporary directory"));
+      return;
+    }
+  filename = xasprintf ("%s/clip.tmp", dirname);
+
   string_map_init (&options);
-  filename = tempnam (NULL, NULL);
   string_map_insert (&options, "output-file", filename);
 
   switch (info)
@@ -884,6 +889,7 @@ clipboard_get_cb (GtkClipboard     *clipboard,
 
   unlink (filename);
   free (filename);
+  rmdir (dirname);
 
   g_list_free (rows);
 }
