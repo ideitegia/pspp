16e66034f4a0f17394ae2ab73d5976d9386a02cf
John Darrington <john@darrington.wattle.id.au>
2010-10-03 12:37:30 +0200

    Constness

--- src/data/casereader-translator.c
+++ src/data/casereader-translator.c
@@ -490,7 +490,7 @@ uniquify (const struct ccase *c, void *aux)
   const union value *current_value = case_data (c, cdr->key);
   const int key_width = var_get_width (cdr->key);
   const double weight = cdr->weight ? case_data (c, cdr->weight)->f : 1.0;
-  const struct ccase *next_case = casereader_peek (cdr->clone, cdr->n + 1);
+  struct ccase *next_case = casereader_peek (cdr->clone, cdr->n + 1);
   int dir = 0;
 
   cdr->n ++;
