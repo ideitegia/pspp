6d6a5acedc41e463e541d6263a3079350518ad06
Ben Pfaff <blp@cs.stanford.edu>
2010-10-10 14:03:33 -0700

    tests: Convert test for overwriting a special file to Autotest framework.

--- tests/automake.mk
+++ tests/automake.mk
@@ -27,7 +27,6 @@ dist_TESTS = \
 	tests/formats/wkday-in.sh \
 	tests/formats/wkday-out.sh \
 	tests/formats/360.sh \
-	tests/bugs/overwrite-special-file.sh \
 	tests/bugs/shbang.sh \
 	tests/bugs/signals.sh \
 	tests/bugs/temporary.sh \
