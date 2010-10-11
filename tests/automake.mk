b74913975ed7b4c83fca98f1f46e7f938a37722d
Ben Pfaff <blp@cs.stanford.edu>
2010-10-10 21:39:45 -0700

    tests: Convert tests for SAVE /KEEP=ALL to Autotest framework.

--- tests/automake.mk
+++ tests/automake.mk
@@ -27,7 +27,6 @@ dist_TESTS = \
 	tests/formats/wkday-in.sh \
 	tests/formats/wkday-out.sh \
 	tests/formats/360.sh \
-	tests/bugs/keep-all.sh \
 	tests/data/datasheet-test.sh \
 	tests/libpspp/sparse-xarray-test.sh \
 	tests/output/paper-size.sh \
