fc1b3d5634cda1856281eeff3be799ac71c3465b
John Darrington <john@darrington.wattle.id.au>
2010-10-23 10:56:14 +0200

    Whitespace changes only

--- src/language/stats/automake.mk
+++ src/language/stats/automake.mk
@@ -11,39 +11,27 @@ src_language_stats_built_sources = \
 	src/language/stats/t-test.c
 
 language_stats_sources = \
-	src/language/stats/aggregate.c \
-	src/language/stats/aggregate.h \
+	src/language/stats/aggregate.c src/language/stats/aggregate.h \
 	src/language/stats/autorecode.c \
-	src/language/stats/binomial.c \
-	src/language/stats/binomial.h \
-	src/language/stats/chisquare.c \
-	src/language/stats/chisquare.h \
+	src/language/stats/binomial.c src/language/stats/binomial.h \
+	src/language/stats/chisquare.c src/language/stats/chisquare.h \
 	src/language/stats/correlations.c \
 	src/language/stats/descriptives.c \
 	src/language/stats/factor.c \
 	src/language/stats/flip.c \
-	src/language/stats/freq.c \
-	src/language/stats/freq.h \
+	src/language/stats/freq.c src/language/stats/freq.h \
 	src/language/stats/glm.c \
-	src/language/stats/kruskal-wallis.c \
-	src/language/stats/kruskal-wallis.h \
-	src/language/stats/npar.c \
-	src/language/stats/npar.h \
-	src/language/stats/npar-summary.c \
-	src/language/stats/npar-summary.h \
+	src/language/stats/kruskal-wallis.c src/language/stats/kruskal-wallis.h \
+	src/language/stats/npar.c src/language/stats/npar.h \
+	src/language/stats/npar-summary.c src/language/stats/npar-summary.h \
 	src/language/stats/oneway.c \
 	src/language/stats/reliability.c \
-	src/language/stats/roc.c \
-	src/language/stats/roc.h \
-	src/language/stats/runs.h \
-	src/language/stats/runs.c \
-	src/language/stats/sign.c \
-	src/language/stats/sign.h \
+	src/language/stats/roc.c src/language/stats/roc.h \
+	src/language/stats/runs.h src/language/stats/runs.c \
+	src/language/stats/sign.c src/language/stats/sign.h \
 	src/language/stats/sort-cases.c \
-	src/language/stats/sort-criteria.c \
-	src/language/stats/sort-criteria.h \
-	src/language/stats/wilcoxon.c \
-	src/language/stats/wilcoxon.h
+	src/language/stats/sort-criteria.c src/language/stats/sort-criteria.h \
+	src/language/stats/wilcoxon.c src/language/stats/wilcoxon.h
 
 all_q_sources += $(src_language_stats_built_sources:.c=.q)
 EXTRA_DIST += $(src_language_stats_built_sources:.c=.q)
