4fc76abeb861faaa36c854dfc86e76d00d8089f4
Ben Pfaff <blp@cs.stanford.edu>
2010-12-11 22:17:01 -0800

    DEBUG XFORM FAIL: Issue an error message as part of transformation.
    PSPP only returns a failure exit status if an error message is issued, but
    DEBUG XFORM FAIL doesn't issue an error message.  This means that, if PSPP
    has no other errors, then it's difficult to tell that the transformation
    really did what it should have.  This commit fixes the potential problem.

--- src/language/xforms/fail.c
+++ src/language/xforms/fail.c
@@ -18,15 +18,14 @@
 
 #include <stdlib.h>
 
-#include <data/case.h>
-#include <data/procedure.h>
-#include <data/transformations.h>
-#include <language/command.h>
-#include <language/lexer/lexer.h>
+#include "data/case.h"
+#include "data/procedure.h"
+#include "data/transformations.h"
+#include "language/command.h"
+#include "language/lexer/lexer.h"
+#include "libpspp/message.h"
 
 static int trns_fail (void *x, struct ccase **c, casenumber n);
-
-
 
 /* A transformation which is guaranteed to fail. */
 
@@ -34,9 +33,9 @@ static int
 trns_fail (void *x UNUSED, struct ccase **c UNUSED,
 	   casenumber n UNUSED)
 {
+  msg (SE, "DEBUG XFORM FAIL transformation executed");
   return TRNS_ERROR;
 }
-
 
 int
 cmd_debug_xform_fail (struct lexer *lexer, struct dataset *ds)
