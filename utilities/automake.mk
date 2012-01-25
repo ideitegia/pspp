a4b62f0df511939a5f9d7d19c12b98a71982516e
Ben Pfaff <blp@cs.stanford.edu>
2012-01-24 22:09:18 -0800

    pspp-dump-sav: Add manpage.

--- utilities/automake.mk
+++ utilities/automake.mk
@@ -1,7 +1,7 @@
 bin_PROGRAMS += utilities/pspp-dump-sav
+man_MANS += utilities/pspp-dump-sav.1
 utilities_pspp_dump_sav_SOURCES = \
 	src/libpspp/integer-format.c \
 	src/libpspp/float-format.c \
 	utilities/pspp-dump-sav.c
 utilities_pspp_dump_sav_CPPFLAGS = $(AM_CPPFLAGS) -DINSTALLDIR=\"$(bindir)\"
-
