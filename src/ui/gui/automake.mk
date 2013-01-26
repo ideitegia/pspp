0cda8ad23eb61de36f13235486fd059e8a1a7d2f
John Darrington <john@darrington.wattle.id.au>
2013-01-26 10:04:02 +0100

    Added a test program for the spread interface

--- src/ui/gui/automake.mk
+++ src/ui/gui/automake.mk
@@ -55,8 +55,10 @@ EXTRA_DIST += \
 
 if HAVE_GUI
 bin_PROGRAMS += src/ui/gui/psppire 
+bin_PROGRAMS += src/ui/gui/spread-test
 
 src_ui_gui_psppire_CFLAGS = $(GTK_CFLAGS) $(GTKSOURCEVIEW_CFLAGS) -Wall -DGDK_MULTIHEAD_SAFE=1
+src_ui_gui_spread_test_CFLAGS = $(GTK_CFLAGS) -Wall -DGDK_MULTIHEAD_SAFE=1
 
 
 src_ui_gui_psppire_LDFLAGS = \
@@ -83,6 +85,16 @@ src_ui_gui_psppire_LDADD = \
 	$(CAIRO_LIBS) \
 	$(LIBINTL) \
 	$(GSL_LIBS)
+
+
+src_ui_gui_spread_test_LDADD = \
+	src/libpspp-core.la \
+	$(GTK_LIBS) \
+	$(GTHREAD_LIBS)
+
+
+src_ui_gui_spread_test_SOURCES = src/ui/gui/sheet-test.c
+
 
 src_ui_gui_psppiredir = $(pkgdatadir)
 
