838cd359986e2b974e2908ca9bb5b40f0cad3f72
John Darrington <john@darrington.wattle.id.au>
2013-05-27 17:12:41 +0200

    PsppSheetView: Remove reference to colormap which is removed in Gtk3

--- src/ui/gui/pspp-sheet-view.c
+++ src/ui/gui/pspp-sheet-view.c
@@ -1477,10 +1477,9 @@ pspp_sheet_view_realize (GtkWidget *widget)
   attributes.height = allocation.height;
   attributes.wclass = GDK_INPUT_OUTPUT;
   attributes.visual = gtk_widget_get_visual (widget);
-  attributes.colormap = gtk_widget_get_colormap (widget);
   attributes.event_mask = GDK_VISIBILITY_NOTIFY_MASK;
 
-  attributes_mask = GDK_WA_X | GDK_WA_Y | GDK_WA_VISUAL | GDK_WA_COLORMAP;
+  attributes_mask = GDK_WA_X | GDK_WA_Y | GDK_WA_VISUAL;
 
   gtk_widget_set_window (widget,
 			 gdk_window_new (gtk_widget_get_parent_window (widget),
@@ -7703,9 +7702,8 @@ _pspp_sheet_view_column_start_drag (PsppSheetView       *tree_view,
       attributes.width = column->allocation.width;
       attributes.height = column->allocation.height;
       attributes.visual = gtk_widget_get_visual (GTK_WIDGET (tree_view));
-      attributes.colormap = gtk_widget_get_colormap (GTK_WIDGET (tree_view));
       attributes.event_mask = GDK_VISIBILITY_NOTIFY_MASK | GDK_EXPOSURE_MASK | GDK_POINTER_MOTION_MASK;
-      attributes_mask = GDK_WA_X | GDK_WA_Y | GDK_WA_VISUAL | GDK_WA_COLORMAP;
+      attributes_mask = GDK_WA_X | GDK_WA_Y | GDK_WA_VISUAL ;
 
       tree_view->priv->drag_window = gdk_window_new (tree_view->priv->bin_window,
 						     &attributes,
