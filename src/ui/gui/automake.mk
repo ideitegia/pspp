577a41e54a202186cf541b2de62e4abc169cab3b
John Darrington <john@darrington.wattle.id.au>
2013-05-28 17:44:22 +0200

    Disable GDK3 deprecation warnings.
    At the moment I am not worried about deprecated constructs, so long as they are valid in Gtk+3.

--- src/ui/gui/automake.mk
+++ src/ui/gui/automake.mk
@@ -57,8 +57,8 @@ if HAVE_GUI
 bin_PROGRAMS += src/ui/gui/psppire 
 noinst_PROGRAMS += src/ui/gui/spreadsheet-test
 
-src_ui_gui_psppire_CFLAGS = $(GTK_CFLAGS) $(GTKSOURCEVIEW_CFLAGS) -Wall -DGDK_MULTIHEAD_SAFE=1
-src_ui_gui_spreadsheet_test_CFLAGS = $(GTK_CFLAGS) -Wall -DGDK_MULTIHEAD_SAFE=1
+src_ui_gui_psppire_CFLAGS = $(GTK_CFLAGS) $(GTKSOURCEVIEW_CFLAGS) -Wall -DGDK_MULTIHEAD_SAFE=1 -DGDK_DISABLE_DEPRECATION_WARNINGS=1
+src_ui_gui_spreadsheet_test_CFLAGS = $(GTK_CFLAGS) -Wall -DGDK_MULTIHEAD_SAFE=1 -DGDK_DISABLE_DEPRECATION_WARNINGS=1
 
 
 src_ui_gui_psppire_LDFLAGS = \
