82c1097ab658e8c13ab3ad439970148defba6e19
John Darrington <john@darrington.wattle.id.au>
2013-09-03 18:59:20 +0200

    Dont try to build in gnumeric/odf reader if the libraries are not installed

--- src/data/spreadsheet-reader.c
+++ src/data/spreadsheet-reader.c
@@ -35,12 +35,16 @@ spreadsheet_destroy (struct spreadsheet *s)
 {
   switch (s->type)
     {
+#ifdef ODF_READ_SUPPORT
     case SPREADSHEET_ODS:
       ods_destroy (s);
       break;
+#endif
+#ifdef GNM_READ_SUPPORT
     case SPREADSHEET_GNUMERIC:
       gnumeric_destroy (s);
       break;
+#endif
     default:
       NOT_REACHED ();
       break;
@@ -51,10 +55,14 @@ spreadsheet_destroy (struct spreadsheet *s)
 struct casereader * 
 spreadsheet_make_reader (struct spreadsheet *s, const struct spreadsheet_read_options *opts)
 {
+#ifdef ODS_READ_SUPPORT
   if ( s->type == SPREADSHEET_ODS)
     return ods_make_reader (s, opts);
+#endif
+#ifdef GNM_READ_SUPPORT
   if ( s->type == SPREADSHEET_GNUMERIC)
     return gnumeric_make_reader (s, opts);
+#endif
 
   return NULL;
 }
@@ -62,11 +70,14 @@ spreadsheet_make_reader (struct spreadsheet *s, const struct spreadsheet_read_op
 const char * 
 spreadsheet_get_sheet_name (struct spreadsheet *s, int n)
 {
+#ifdef ODF_READ_SUPPORT
   if ( s->type == SPREADSHEET_ODS)
     return ods_get_sheet_name (s, n);
-
+#endif
+#ifdef GNM_READ_SUPPORT
   if ( s->type == SPREADSHEET_GNUMERIC)
     return gnumeric_get_sheet_name (s, n);
+#endif
 
   return NULL;
 }
@@ -74,11 +85,15 @@ spreadsheet_get_sheet_name (struct spreadsheet *s, int n)
 char * 
 spreadsheet_get_sheet_range (struct spreadsheet *s, int n)
 {
+#ifdef ODF_READ_SUPPORT
   if ( s->type == SPREADSHEET_ODS)
     return ods_get_sheet_range (s, n);
+#endif
 
+#ifdef GNM_READ_SUPPORT
   if ( s->type == SPREADSHEET_GNUMERIC)
     return gnumeric_get_sheet_range (s, n);
+#endif
 
   return NULL;
 }
