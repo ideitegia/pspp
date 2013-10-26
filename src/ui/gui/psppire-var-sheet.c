ideitegia/pspp

commit fd308174a81b1702426720536acd3aa4a3021801

Corrected logical errors in tests for error conditions.
Fixed several places where && had bee used where || was intended
and vici-versa.

Reported-By: David Binderman <dcb314@hotmail.com>

src/ui/gui/psppire-var-sheet.c
@@ -591,7 +591,7 @@ get_string_width (PsppSheetView *treeview, GtkCellRenderer *renderer,
 }
 
 static gint
-get_monospace_width (PsppSheetView *treeview, GtkCellRenderer *renderer,
+get_monospac_width (PsppSheetView *treeview, GtkCellRenderer *renderer,
                      size_t char_cnt)
 {
   struct string s;

src/data/data-in.c
@@ -919,7 +919,7 @@ parse_year (struct data_in *i, long *year, size_t max_digits)
       else
         *year += epoch_century + 100;
     }
-  if (*year >= 1582 || *year <= 19999)
+  if (*year >= 1582 && *year <= 19999)
     return NULL;
 
   return xasprintf (_("Year (%ld) must be between 1582 and 19999."), *year);

src/language/lexer/q2c.c
@@ -1729,7 +1729,7 @@ dump_parser (int persistent)
   dump (0, "static int");
   dump (0, "parse_%s (struct lexer *lexer, struct dataset *ds%s, struct cmd_%s *p, void *aux UNUSED)",
         make_identifier (cmdname),
-	(def && ( def->type == SBC_VARLIST && def->type == SBC_CUSTOM))?"":" UNUSED",
+	(def && ( def->type == SBC_VARLIST || def->type == SBC_CUSTOM))?"":" UNUSED",
 	make_identifier (cmdname));
   dump (1, "{");
 

src/language/stats/glm.c
@@ -267,7 +267,7 @@ cmd_glm (struct lexer *lexer, struct dataset *ds)
 	    }
 
 	  glm.ss_type = lex_integer (lexer);
-	  if (1 > glm.ss_type  && 3 < glm.ss_type )
+	  if (1 > glm.ss_type  ||  3 < glm.ss_type )
 	    {
 	      msg (ME, _("Only types 1, 2 & 3 sums of squares are currently implemented"));
 	      goto error;

commit da8c6e44f6e8acaa04887901da4d835437221ccf

psppire-var-sheet: Correct typo that breaks the build.
Commit fd308174a81 (Corrected logical errors in tests for error
conditions.) introduced a typo in a function name.  This commit corrects
it.

src/ui/gui/psppire-var-sheet.c
@@ -591,7 +591,7 @@ get_string_width (PsppSheetView *treeview, GtkCellRenderer *renderer,
 }
 
 static gint
-get_monospac_width (PsppSheetView *treeview, GtkCellRenderer *renderer,
+get_monospace_width (PsppSheetView *treeview, GtkCellRenderer *renderer,
                      size_t char_cnt)
 {
   struct string s;