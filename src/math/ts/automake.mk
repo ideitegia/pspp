8ebdf8a26f4ed036c9621810e0561278090d921e
Ben Pfaff <blp@cs.stanford.edu>
2013-12-23 08:53:32 -0800

    src/math/ts: Remove entire directory, which is unused.
    Nothing actually builds or uses anything in this directory.

--- src/math/ts/automake.mk
+++
@@ -1,9 +0,0 @@
-## Process this file with automake to produce Makefile.in  -*- makefile -*-
-
-noinst_LTLIBRARIES += src/math/ts/libpspp_ts.la
-
-src_math_ts_libpspp_ts_la_SOURCES = \
-	src/math/ts/innovations.c \
-	src/math/ts/innovations.h
-
-EXTRA_DIST += src/math/ts/OChangeLog
