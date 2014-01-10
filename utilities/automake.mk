d32245378988468e419654b9fe06fef066adfda1
Ben Pfaff <blp@cs.stanford.edu>
2014-01-09 21:08:10 -0800

    pspp-convert: Fix Mac OS build by eliminating redundant linking.
    The Mach-O object format used on Mac OS handles redundant linking against
    a shared object badly, whereas ELF doesn't have a problem, so this commit
    fixes a crash on Mac OS.
    
    Reported by Jeremy Lavergne.

--- utilities/automake.mk
+++ utilities/automake.mk
@@ -10,7 +10,4 @@ bin_PROGRAMS += utilities/pspp-convert
 dist_man_MANS += utilities/pspp-convert.1
 utilities_pspp_convert_SOURCES = utilities/pspp-convert.c
 utilities_pspp_convert_CPPFLAGS = $(AM_CPPFLAGS) -DINSTALLDIR=\"$(bindir)\"
-utilities_pspp_convert_LDADD = \
-	src/libpspp/liblibpspp.la \
-	src/libpspp-core.la \
-	gl/libgl.la
+utilities_pspp_convert_LDADD = src/libpspp-core.la
