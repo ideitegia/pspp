ideitegia/pspp

commit 14f4522a17db23e67a6fa17876633cc6260cb42b

Change many %g format specifiers to %.*g with precision DBL_DIG + 1.
The high precision should ensure that in most cases the value is formatted
with enough digits to fully express the internal precision.  Without this
change, %g by itself only expresses 6 digits of precision, so that an
integer value like 99999999 is formatted as 1e8.  This fixes the problem.

This commit adds a test to MISSING VALUES because this was in particular
reported as a problem.

Thanks to Andre Müller <Andre.Mueller@gesis.org> for reporting this issue.

src/ui/gui/psppire-dialog-action-logistic.c
@@ -1,5 +1,5 @@
 /* PSPPIRE - a graphical user interface for PSPP.
-   Copyright (C) 2008, 2010, 2011, 2012  Free Software Foundation
+   Copyright (C) 2008, 2010, 2011, 2012, 2014  Free Software Foundation
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -19,6 +19,8 @@
 
 #include "psppire-dialog-action-logistic.h"
 #include "psppire-value-entry.h"
+
+#include <float.h>
 
 #include "dialog-common.h"
 #include "helper.h"
@@ -165,13 +167,14 @@ generate_syntax (PsppireDialogAction *a)
 
   ds_put_cstr (&str, "\n\t/CRITERIA =");
 
-  syntax_gen_pspp (&str, " CUT(%g)", rd->cut_point);
+  syntax_gen_pspp (&str, " CUT(%.*g)", DBL_DIG + 1, rd->cut_point);
 
   syntax_gen_pspp (&str, " ITERATE(%d)", rd->max_iterations);
 
   if (rd->conf)
     {
-      syntax_gen_pspp (&str, "\n\t/PRINT = CI(%g)", rd->conf_level);
+      syntax_gen_pspp (&str, "\n\t/PRINT = CI(%.*g)",
+                       DBL_DIG + 1, rd->conf_level);
     }
 
   if (rd->constant) 

src/language/dictionary/sys-file-info.c
@@ -1,5 +1,5 @@
 /* PSPP - a program for statistical analysis.
-   Copyright (C) 1997-9, 2000, 2006, 2009, 2010, 2011, 2012, 2013 Free Software Foundation, Inc.
+   Copyright (C) 1997-9, 2000, 2006, 2009, 2010, 2011, 2012, 2013, 2014 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -17,6 +17,7 @@
 #include <config.h>
 
 #include <ctype.h>
+#include <float.h>
 #include <stdlib.h>
 
 #include "data/attributes.h"
@@ -560,11 +561,13 @@ describe_variable (const struct variable *v, struct tab_table *t, int r,
           double x, y;
           mv_get_range (mv, &x, &y);
           if (x == LOWEST)
-            cp += sprintf (cp, "LOWEST THRU %g", y);
+            cp += sprintf (cp, "LOWEST THRU %.*g", DBL_DIG + 1, y);
           else if (y == HIGHEST)
-            cp += sprintf (cp, "%g THRU HIGHEST", x);
+            cp += sprintf (cp, "%.*g THRU HIGHEST", DBL_DIG + 1, x);
           else
-            cp += sprintf (cp, "%g THRU %g", x, y);
+            cp += sprintf (cp, "%.*g THRU %.*g",
+                           DBL_DIG + 1, x,
+                           DBL_DIG + 1, y);
           cnt++;
         }
       for (i = 0; i < mv_n_values (mv); i++)
@@ -573,7 +576,7 @@ describe_variable (const struct variable *v, struct tab_table *t, int r,
           if (cnt++ > 0)
             cp += sprintf (cp, "; ");
           if (var_is_numeric (v))
-            cp += sprintf (cp, "%g", value->f);
+            cp += sprintf (cp, "%.*g", DBL_DIG + 1, value->f);
           else
             {
               int width = var_get_width (v);

src/language/lexer/value-parser.c
@@ -1,5 +1,5 @@
 /* PSPP - a program for statistical analysis.
-   Copyright (C) 2005, 2006, 2009, 2010, 2011 Free Software Foundation, Inc.
+   Copyright (C) 2005, 2006, 2009, 2010, 2011, 2014 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -18,6 +18,7 @@
 
 #include "value-parser.h"
 
+#include <float.h>
 #include <stdbool.h>
 
 #include "data/data-in.h"
@@ -62,15 +63,15 @@ parse_num_range (struct lexer *lexer,
       if (*y < *x)
         {
           double t;
-          msg (SW, _("The high end of the range (%g) is below the low end (%g).  "
-                     "The range will be treated as if reversed."),
-               *y, *x);
+          msg (SW, _("The high end of the range (%.*g) is below the low end "
+                     "(%.*g).  The range will be treated as if reversed."),
+               DBL_DIG + 1, *y, DBL_DIG + 1, *x);
           t = *x;
           *x = *y;
           *y = t;
         }
       else if (*x == *y)
-        msg (SW, _("Ends of range are equal (%g)."), *x);
+        msg (SW, _("Ends of range are equal (%.*g)."), DBL_DIG + 1, *x);
 
       return true;
     }

src/language/stats/autorecode.c
@@ -1,5 +1,5 @@
 /* PSPP - a program for statistical analysis.
-   Copyright (C) 1997-9, 2000, 2009, 2010, 2012, 2013 Free Software Foundation, Inc.
+   Copyright (C) 1997-9, 2000, 2009, 2010, 2012, 2013, 2014 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -16,6 +16,7 @@
 
 #include <config.h>
 
+#include <float.h>
 #include <stdlib.h>
 
 #include "data/case.h"
@@ -323,7 +324,7 @@ cmd_autorecode (struct lexer *lexer, struct dataset *ds)
                                              str, src_width);
 	    }
 	  else
-	    recoded_value = c_xasprintf ("%g", from->f);
+	    recoded_value = c_xasprintf ("%.*g", DBL_DIG + 1, from->f);
 	  
 	  /* Remove trailing whitespace */
           len = strlen (recoded_value);

src/language/stats/binomial.c
@@ -1,5 +1,5 @@
 /* PSPP - a program for statistical analysis.
-   Copyright (C) 2006, 2009, 2010, 2011 Free Software Foundation, Inc.
+   Copyright (C) 2006, 2009, 2010, 2011, 2014 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -18,6 +18,7 @@
 
 #include "language/stats/binomial.h"
 
+#include <float.h>
 #include <gsl/gsl_cdf.h>
 #include <gsl/gsl_randist.h>
 
@@ -202,7 +203,8 @@ binomial_execute (const struct dataset *ds,
 
 	  if ( bst->cutpoint != SYSMIS)
 	    {
-	      ds_put_format (&catstr[0], "<= %g", bst->cutpoint);
+	      ds_put_format (&catstr[0], "<= %.*g",
+                             DBL_DIG + 1, bst->cutpoint);
 	    }
           else
             {

src/language/stats/cochran.c
@@ -1,5 +1,5 @@
 /* PSPP - a program for statistical analysis.
-   Copyright (C) 2010, 2011 Free Software Foundation, Inc.
+   Copyright (C) 2010, 2011, 2014 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -18,6 +18,7 @@
 
 #include "language/stats/cochran.h"
 
+#include <float.h>
 #include <gsl/gsl_cdf.h>
 #include <stdbool.h>
 
@@ -169,8 +170,10 @@ show_freqs_box (const struct one_sample_test *ost, const struct cochran *ct)
   tab_joint_text (table, 1, 0, 2, 0,
 		  TAT_TITLE | TAB_CENTER, _("Value"));
 
-  tab_text_format (table, 1, 1, 0, _("Success (%g)"), ct->success);
-  tab_text_format (table, 2, 1, 0, _("Failure (%g)"), ct->failure);
+  tab_text_format (table, 1, 1, 0, _("Success (%.*g)"),
+                   DBL_DIG + 1, ct->success);
+  tab_text_format (table, 2, 1, 0, _("Failure (%.*g)"),
+                   DBL_DIG + 1, ct->failure);
 
   tab_hline (table, TAL_2, 0, tab_nc (table) - 1, column_headers);
   tab_vline (table, TAL_2, row_headers, 0, tab_nr (table) - 1);

src/language/stats/crosstabs.q
@@ -1,5 +1,5 @@
 /* PSPP - a program for statistical analysis.
-   Copyright (C) 1997-9, 2000, 2006, 2009, 2010, 2011, 2012, 2013 Free Software Foundation, Inc.
+   Copyright (C) 1997-9, 2000, 2006, 2009, 2010, 2011, 2012, 2013, 2014 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -29,6 +29,7 @@
 #include <config.h>
 
 #include <ctype.h>
+#include <float.h>
 #include <gsl/gsl_cdf.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -1944,8 +1945,8 @@ display_risk (struct pivot_table *pt, struct tab_table *risk)
 	case 1:
 	case 2:
 	  if (var_is_numeric (rv))
-	    sprintf (buf, _("For cohort %s = %g"),
-		     var_to_string (rv), pt->rows[i - 1].f);
+	    sprintf (buf, _("For cohort %s = %.*g"),
+		     var_to_string (rv), DBL_DIG + 1, pt->rows[i - 1].f);
 	  else
 	    sprintf (buf, _("For cohort %s = %.*s"),
 		     var_to_string (rv),

src/language/stats/descriptives.c
@@ -1,5 +1,5 @@
 /* PSPP - a program for statistical analysis.
-   Copyright (C) 1997-2000, 2009-2013 Free Software Foundation, Inc.
+   Copyright (C) 1997-2000, 2009-2014 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -16,6 +16,7 @@
 
 #include <config.h>
 
+#include <float.h>
 #include <limits.h>
 #include <math.h>
 #include <stdlib.h>
@@ -1002,17 +1003,18 @@ display (struct dsc_proc *dsc)
 
       nc = 0;
       tab_text (t, nc++, i + 1, TAB_LEFT, var_to_string (dv->v));
-      tab_text_format (t, nc++, i + 1, 0, "%g", dv->valid);
+      tab_text_format (t, nc++, i + 1, 0, "%.*g", DBL_DIG + 1, dv->valid);
       if (dsc->format == DSC_SERIAL)
-	tab_text_format (t, nc++, i + 1, 0, "%g", dv->missing);
+	tab_text_format (t, nc++, i + 1, 0, "%.*g", DBL_DIG + 1, dv->missing);
 
       for (j = 0; j < DSC_N_STATS; j++)
 	if (dsc->show_stats & (1ul << j))
 	  tab_double (t, nc++, i + 1, TAB_NONE, dv->stats[j], NULL);
     }
 
-  tab_title (t, _("Valid cases = %g; cases with missing value(s) = %g."),
-	     dsc->valid, dsc->missing_listwise);
+  tab_title (t, _("Valid cases = %.*g; cases with missing value(s) = %.*g."),
+	     DBL_DIG + 1, dsc->valid,
+             DBL_DIG + 1, dsc->missing_listwise);
 
   tab_submit (t);
 }

commit 78cffcbf683cd66a6717d40854825d731fd4f01d

Avoid crash in psppire-dialog-action-logistic.c
Commit 14f4522a17db23e67a6fa17876633cc6260cb42b introduced some
printf directives which syntax_gen_pspp did not support.  This
resulted in an assertion failure.

This change reverts the changes which caused the problem.

src/ui/gui/psppire-dialog-action-logistic.c
@@ -1,5 +1,5 @@
 /* PSPPIRE - a graphical user interface for PSPP.
-   Copyright (C) 2008, 2010, 2011, 2012, 2014  Free Software Foundation
+   Copyright (C) 2008, 2010, 2011, 2012  Free Software Foundation
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -19,8 +19,6 @@
 
 #include "psppire-dialog-action-logistic.h"
 #include "psppire-value-entry.h"
-
-#include <float.h>
 
 #include "dialog-common.h"
 #include "helper.h"
@@ -167,14 +165,13 @@ generate_syntax (PsppireDialogAction *a)
 
   ds_put_cstr (&str, "\n\t/CRITERIA =");
 
-  syntax_gen_pspp (&str, " CUT(%.*g)", DBL_DIG + 1, rd->cut_point);
+  syntax_gen_pspp (&str, " CUT(%g)", rd->cut_point);
 
   syntax_gen_pspp (&str, " ITERATE(%d)", rd->max_iterations);
 
   if (rd->conf)
     {
-      syntax_gen_pspp (&str, "\n\t/PRINT = CI(%.*g)",
-                       DBL_DIG + 1, rd->conf_level);
+      syntax_gen_pspp (&str, "\n\t/PRINT = CI(%g)", rd->conf_level);
     }
 
   if (rd->constant) 