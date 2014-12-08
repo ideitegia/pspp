47f9412c378ef8e0bcce43566f72caa3b856580b
Ben Pfaff <blp@cs.stanford.edu>
2014-12-07 22:10:54 -0800

    lexer: Include <unistd.h> to ensure 'read' is consistently replaced.
    On some platforms, Gnulib #defines read to rpl_read.  The lexer uses
    an unreleated function pointer member named 'read' in struct
    lex_reader_class.  It doesn't really matter whether this name gets
    replaced with rpl_read as long as it always happens consistently.
    Adding "#include <unistd.h>" to lexer.h should ensure that.
    
    Reported by Harry Thijssen.

--- src/language/lexer/lexer.h
+++ src/language/lexer/lexer.h
@@ -1,5 +1,5 @@
 /* PSPP - a program for statistical analysis.
-   Copyright (C) 1997-9, 2000, 2010, 2011, 2013 Free Software Foundation, Inc.
+   Copyright (C) 1997-9, 2000, 2010, 2011, 2013, 2014 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -19,6 +19,7 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <unistd.h>
 
 #include "data/identifier.h"
 #include "data/variable.h"
